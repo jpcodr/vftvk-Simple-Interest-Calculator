6d85699b8160fe94f56816b5a6822f72258b2c97
Pablo Macias <jupamara@gmail.com>
2021-06-10 18:16:53 +0000

    highlight amounts and changed title

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ function compute() {
     const result = document.getElementById('result');
 
     result.innerHTML = `
-        If you deposit $${principalValue}<br />
-        at an interest rate of ${rate}%<br />
-        You will receive an amount of $${interest}<br />
-        in the year ${futureYear}
+        If you deposit <span class='highlight'>$${principalValue}</span><br />
+        at an interest rate of <span class='highlight'>${rate}%</span><br />
+        You will receive an amount of <span class='highlight'>$${interest}</span><br />
+        in the year <span class='highlight'>${futureYear}</span>
     `;
     result.style.display = 'block';
 }
